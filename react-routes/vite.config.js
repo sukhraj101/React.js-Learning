179d36740416522e2ff33b2b35679325006f7214
sukhrajsingh101 <sukhrajsingh101@gmail.com>
2025-10-02 21:02:09 +0530

    React hooks - 3

--- react-routes/vite.config.js
+++ react-routes/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: false, // Disable Hot Module Replacement (HMR)
+    headers: {
+      'Content-Security-Policy': "script-src 'self' 'unsafe-eval'",
+    },
+  },
+  build: {
+    sourcemap: false, // Disable source maps, which may rely on eval()
+  },
 })
